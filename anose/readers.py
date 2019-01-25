66826cc955405b7640a1360c3cd0959c3b797e0b
Bohdan Dmytriv <dmytriv@quintagroup.org>
2019-01-25 17:47:19 +0200

    Basic usage of pytest
    Collected output errors test data

---
+++ anose/readers.py
@@ -0,0 +1,9 @@
+import json
+
+def read_file_at_once(location):
+    file_data = None
+
+    with open(location, 'r') as f:
+        file_data = f.read()
+    
+    return file_data
---
+++ tests/readers_test.py
@@ -0,0 +1,7 @@
+from tempfile import NamedTemporaryFile
+
+
+class TestReadFileAtOnce(object):
+
+    def test_ok(self):
+        pass
