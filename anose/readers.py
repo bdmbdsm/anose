7e8f7f84000b0ea13eb13a57e064bc6d9ef1b5a2
Bohdan Dmytriv <dmytriv@quintagroup.org>
2019-01-28 16:21:51 +0200

    Fix style issues

--- anose/readers.py
+++ anose/readers.py
@@ -1,9 +1,7 @@
-import json
-
 def read_file_at_once(location):
     file_data = None
 
     with open(location, 'r') as f:
         file_data = f.read()
-    
+
     return file_data
--- tests/readers_test.py
+++ tests/readers_test.py
@@ -1,11 +1,12 @@
 from anose.readers import read_file_at_once
 
+
 class TestReadFileAtOnce(object):
 
     def test_ok(self, tmp_file_name):
         TESTDATA = 'otrpotr'
         with open(tmp_file_name, 'w') as f:
             f.write(TESTDATA)
-        
+
         res = read_file_at_once(tmp_file_name)
         assert res == TESTDATA
